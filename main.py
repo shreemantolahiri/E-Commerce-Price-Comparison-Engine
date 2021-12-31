d6991ddd6ecf10e59c0f3e6397a9ad54d1ea4660
LAKSHAY BHARADWAJ <94477450+JOSHI53@users.noreply.github.com>
2022-01-01 04:44:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,130 @@
 import requests
 from bs4 import BeautifulSoup
-URL="https://www.amazon.in/dp/B074WZJ4MF/ref=redir_mobile_desktop?_encoding=UTF8&aaxitk=8bc2212eee66e1c1bdca057df16f612f&hsa_cr_id=2722802130102&pd_rd_plhdr=t&pd_rd_r=135b3806-45ad-402d-9df7-0f14d458f874&pd_rd_w=19o2S&pd_rd_wg=TBmei&ref_=sbx_be_s_sparkle_mcd_asin_0_title"
+
+name=input("ENTER THE SEARCH: ")
+# name=""
+name=name.replace(" ","+")
+
+#_4rR01T---VERTICAL   s1Q9rs----HORIZONTAL
 HEADERS={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"}
+URL=""
+flip=(f"https://www.flipkart.com/search?q={name}&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off")
+    
 
-def getPrice():
+def flipkart():
+    URL=flip
     page= requests.get(URL,headers=HEADERS)
+    soup=BeautifulSoup(page.content,'html.parser') 
+    class1='_4rR01T'
+    anchor=soup.findAll(class_=class1)
+    if len(anchor)==0:
+        class1='s1Q9rs'
+        info=[]
+        anchor=soup.findAll(class_=class1)
+        if len(anchor) == 0:
+            class1 = 's1Q9rs'
+        anchor = soup.findAll(class_=class1)
+        info = []
+        #for row in soup.findAll('a', attrs={'class': class1}):
+            #print(type(row.a[title]))
+            # info.append(row.a['title'])
 
-    
-    soup=BeautifulSoup(page.content,'html.parser')
-    
-    title=soup.find(class_= "a-price-whole").get_text()
-    
-    print(title)
 
+        ctr = 0
+        for i in anchor:
+            
+
+            if ctr < 5:
+
+                i = str(i)
+                # print(i,class1)
+
+                L = i.split('title="')
+                fin = L[1].split('>')
+                fin1 = fin[0].rstrip('"')
+
+                item_names = fin1.upper()
+                # print(item_names)
+                
+                # verification
+                search = name.upper()
+                search = search.split('+')
+                # print(search)
+                checker = 0
+
+                for i in range(len(search)):
+
+                    if search[i] in item_names:
+                        checker += 1
+
+        
+                    else:
+                        
+                        break
+
+                    if checker == len(search):
+                        print(item_names)
+                        ctr += 1
+        
+        if ctr == 0:
+        
+            for i in anchor:
+        
+                if ctr < 5:
+        
+                    i = str(i)
+                    L = i.split('title="')
+                    fin = L[1].split('>')
+                    fin1 = fin[0].rstrip('"')
+        
+                    item_names = fin1.upper()
+                    print(item_names)
+                    ctr += 1    
+        
+    
+    else: 
+        ctr=0
+        for i in anchor:
+
+            if ctr<5:
+
+                i = str(i)
+                # print(i,class1)
+                new = i.lstrip(f'<div class="{class1}">').rstrip('</div>')
+                item_names=new.upper()
+                # print(item_names)
+                # verification
+                search=name.upper()
+                search=search.split('+')
+                # print(search)
+                checker=0
+
+                for i in range(len(search)):
+
+
+                    if search[i] in item_names:
+                        checker+=1
+                        # print("he")
+
+                    else:
+                        # print("she")
+                        break
+
+                    if checker==len(search):
+                        print(item_names)
+                        ctr += 1
+
+            #incase no output, verification method is skipped
+            if ctr==0:
+
+                for i in anchor:
+
+                    if ctr < 5:
+
+                        i = str(i)
+                        new = i.lstrip(f'<div class="{class1}">').rstrip('</div>')
+                        item_names = new.upper()
+                        print(item_names)
+                        ctr+=1
 if __name__=="__main__":
-    getPrice()
+    flipkart()
